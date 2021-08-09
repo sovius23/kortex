a551e7af2539bc515dc5b2fc2ea11a9b0d9e9373
ilia <il.vsl0110@gmail.com>
2021-08-09 15:46:23 +0300

    change postgres to sqllite3

--- easyCards/settings.py
+++ easyCards/settings.py
@@ -125,15 +125,10 @@ WSGI_APPLICATION = 'easyCards.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'evgenapp3',
-        'USER' : 'postgres',
-        'PASSWORD' : 'Ilvas2006',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
