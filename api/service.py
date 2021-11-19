f288aa9eb6bf1a0dc49c6f34f0848a9fa6dd86e1
sovius23 <sovius5@mail.ru>
2021-11-19 21:16:14 +0300

    half-path to form report

--- api/service.py
+++ api/service.py
@@ -11,21 +11,22 @@ def report_handler(request):
     table_general = doc.tables[0]
     json_from_post = json.loads(request.POST["post"])
     list = ["Дорожная одежда", "Земляное полотно", "Мостовые сооружения"]
-
-    fill_general_table(json_from_post, table_general)
+    types = [types["type"] for types in json_from_post["plots"]]
 
     object_table_to_add = DocxTemplate(filepath + "DeformationReportObjectTable.docx")
     for table in object_table_to_add.tables:
-        if is_table_suitable(table, list):
-            for row2 in json_from_post["plots"]:
-                for row3 in table.rows[2:]:
-                    if row3.cells[0].text.strip() == row2["type"]:
-                        row3.cells[2].text = row2["date"]
-                        row3.cells[4].text = row2["type"]
-    for table in object_table_to_add.tables:
-        for empty_row in table.rows:
-            if empty_row.cells[2].text.strip() == "":  # and empty_row.cells[4].text.strip():
-                empty_row._element.getparent().remove(empty_row._element)
+        is_table_suitable(table, list)
+    for plot in json_from_post["plots"]:
+        i = 1
+        fill_general_table(json_from_post, plot, table_general, i)
+        for table in object_table_to_add.tables:
+            for row in table.rows[2:]:
+                if row.cells[0].text.strip() == plot["type"]:
+                    row.cells[2].text = plot["date"]
+                    row.cells[4].text = plot["type"]
+        i += 1
+
+    clean_empty_rows(object_table_to_add)
     context = {"table_number": str(1)}
     object_table_to_add.render(context)
     composer.append(object_table_to_add)
@@ -42,15 +43,12 @@ def report_handler(request):
     doc.save("gen.docx")
 
 
-def fill_general_table(json_from_post, table_general):
-    i = 1
-    for plot in json_from_post["plots"]:
-        row_cells = table_general.add_row().cells
-        row_cells[0].text = str(i)
-        row_cells[1].text = json_from_post["city"] + " " + plot["name"]
-        row_cells[2].text = plot["date"]
-        row_cells[3].text = plot["type"]
-        i += 1
+def fill_general_table(json_from_post, plot, table_general, i):
+    row_cells = table_general.add_row().cells
+    row_cells[0].text = str(i)
+    row_cells[1].text = json_from_post["city"] + " " + plot["name"]
+    row_cells[2].text = plot["date"]
+    row_cells[3].text = plot["type"]
 
 
 def is_table_suitable(table, list):
@@ -58,3 +56,10 @@ def is_table_suitable(table, list):
         if not table.rows[0].cells[0].text.strip().find(partition) == -1:
             return True
     table._element.getparent().remove(table._element)
+
+
+def clean_empty_rows(object_table_to_add):
+    for table in object_table_to_add.tables:
+        for empty_row in table.rows:
+            if empty_row.cells[2].text.strip() == "":  # and empty_row.cells[4].text.strip():
+                empty_row._element.getparent().remove(empty_row._element)
