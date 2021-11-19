c5165d724475ddfea80b946250a0a46b3bb7f455
sovius23 <sovius5@mail.ru>
2021-11-19 21:29:24 +0300

    half-path to form report

--- api/service.py
+++ api/service.py
@@ -6,41 +6,37 @@ import json
 
 def report_handler(request):
     filepath = 'api/src/docs/'
-    doc = DocxTemplate(filepath + "DeformationReportMain.docx")
-    composer = Composer(doc)
-    table_general = doc.tables[0]
+    main_doc = DocxTemplate(filepath + "DeformationReportMain.docx")
+    object_table_to_add = DocxTemplate(filepath + "DeformationReportObjectTable.docx")
+
+    composer = Composer(main_doc)
+    table_general = main_doc.tables[0]
+
     json_from_post = json.loads(request.POST["post"])
     list = ["Дорожная одежда", "Земляное полотно", "Мостовые сооружения"]
-    types = [types["type"] for types in json_from_post["plots"]]
-
-    object_table_to_add = DocxTemplate(filepath + "DeformationReportObjectTable.docx")
-    for table in object_table_to_add.tables:
-        is_table_suitable(table, list)
+    #types = [types["type"] for types in json_from_post["plots"]]
+    i = 1
     for plot in json_from_post["plots"]:
-        i = 1
+
         fill_general_table(json_from_post, plot, table_general, i)
-        for table in object_table_to_add.tables:
-            for row in table.rows[2:]:
-                if row.cells[0].text.strip() == plot["type"]:
-                    row.cells[2].text = plot["date"]
-                    row.cells[4].text = plot["type"]
         i += 1
+        for table in object_table_to_add.tables:
+            is_table_suitable(table, list)
+            fill_table_rows(table, plot)
+
 
     clean_empty_rows(object_table_to_add)
     context = {"table_number": str(1)}
     object_table_to_add.render(context)
     composer.append(object_table_to_add)
 
-    for par in doc.paragraphs:
-        if "Таблица по объекту" in par.text:
-            br = par.insert_paragraph_before("")
-            run = br.add_run()
-            run.add_break(WD_BREAK.PAGE)
+    add_page_break(main_doc)
+
 
     context = {'num': "7", "city": json_from_post['city'], "time": json_from_post['time'],
                "date": json_from_post['date'], "whos": json_from_post['whos']}
-    doc.render(context)
-    doc.save("gen.docx")
+    main_doc.render(context)
+    main_doc.save("gen.docx")
 
 
 def fill_general_table(json_from_post, plot, table_general, i):
@@ -63,3 +59,17 @@ def clean_empty_rows(object_table_to_add):
         for empty_row in table.rows:
             if empty_row.cells[2].text.strip() == "":  # and empty_row.cells[4].text.strip():
                 empty_row._element.getparent().remove(empty_row._element)
+
+
+def fill_table_rows(table, plot):
+    for row in table.rows[2:]:
+        if row.cells[0].text.strip() == plot["type"]:
+            row.cells[2].text = plot["date"]
+            row.cells[4].text = plot["type"]
+
+def add_page_break(main_doc):
+    for par in main_doc.paragraphs:
+        if "Таблица по объекту" in par.text:
+            br = par.insert_paragraph_before("")
+            run = br.add_run()
+            run.add_break(WD_BREAK.PAGE)
